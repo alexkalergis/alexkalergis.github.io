06ad22a77b84efecf2efef7a529c8b3e70131c05
Alex Kalergis <105602973+alexkalergis@users.noreply.github.com>
2025-05-28 14:23:58 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',           // <-- Required for static HTML export
-  trailingSlash: true,        // <-- Ensures every route has a trailing slash (important for GitHub Pages)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
